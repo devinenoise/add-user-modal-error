f7ae7ad378d6ee9773877afc0579052a35fc0028
Kyle Devine <devinenoise@gmail.com>
2021-05-19 13:14:34 -0700

    button

---
+++ src/components/UI/Button.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import classes from './Button.module.css';
+
+const Button = props => {
+  return (
+    // if props.type is defined it will be used otherwise default to button
+    <button
+      className={classes.button}
+      type={props.type || 'button'}
+      onclick={props.onClick}
+    >
+      {props.children}
+    </button>
+  );
+};
+
+export default Button;
