051afc3fd77356f8b1eef2189b7ec1da7f2abbaa
Kyle Devine <devinenoise@gmail.com>
2021-05-20 12:08:32 -0700

    error modal

--- src/App.js
+++ src/App.js
@@ -4,11 +4,14 @@ import AddUser from './components/Users/AddUser';
 import UsersList from './components/Users/UsersList';
 
 function App() {
+  // initializing the state with an empty array
   const [usersList, setUsersList] = useState([]);
 
+  // handler looks for a username and age argument
   const addUserHandler = (userName, userAge) => {
     setUsersList(prevUsersList => {
       return [
+        // spreads the prevUsers state and insert a name,age,id into the array
         ...prevUsersList,
         { name: userName, age: userAge, id: Math.random().toString() }
       ];
--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,23 +1,35 @@
 import React, { useState } from 'react';
 import Card from '../UI/Card';
+import Modal from '../UI/Modal';
 import classes from './AddUser.module.css';
 import Button from '../UI/Button';
 
 const AddUser = props => {
   const [enteredUsername, setEnteredUsername] = useState('');
   const [enteredAge, setEnteredAge] = useState('');
+  const [error, setError] = useState();
 
   const addUserHandler = event => {
     event.preventDefault();
     // checking if the input is empty
     if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
+      setError({
+        title: 'Invalid Input',
+        message: 'Username/Age inputs cannot be blank'
+      });
       return;
     }
     // checking if age is above 1 and ensuring it is a number
     if (+enteredAge < 1) {
+      setError({
+        title: 'Invalid Age',
+        message: 'Please enter a valid age'
+      });
       return;
     }
+    // props is passed down from App.js to send the username and age to lift the state
     props.onAddUser(enteredUsername, enteredAge);
+    // reset the input field to a blank string for two-way binding
     setEnteredUsername('');
     setEnteredAge('');
   };
@@ -25,30 +37,45 @@ const AddUser = props => {
   const usernameChangeHandler = event => {
     setEnteredUsername(event.target.value);
   };
+
   const ageChangeHandler = event => {
     setEnteredAge(event.target.value);
   };
 
+  const errorHandler = () => {
+    setError(null);
+  };
+
   return (
-    <Card className={classes.input}>
-      <form onSubmit={addUserHandler}>
-        <label htmlFor='username'>Username</label>
-        <input
-          id='username'
-          type='text'
-          onChange={usernameChangeHandler}
-          value={enteredUsername}
+    <div>
+      {/* conditional to check if there is an error, and if so display the Modal */}
+      {error && (
+        <Modal
+          title={error.title}
+          message={error.message}
+          onConfirm={errorHandler}
         />
-        <label htmlFor='age'>Age (Years)</label>
-        <input
-          id='age'
-          type='number'
-          onChange={ageChangeHandler}
-          value={enteredAge}
-        />
-        <Button type='submit'>Add User</Button>
-      </form>
-    </Card>
+      )}
+      <Card className={classes.input}>
+        <form onSubmit={addUserHandler}>
+          <label htmlFor='username'>Username</label>
+          <input
+            id='username'
+            type='text'
+            onChange={usernameChangeHandler}
+            value={enteredUsername}
+          />
+          <label htmlFor='age'>Age (Years)</label>
+          <input
+            id='age'
+            type='number'
+            onChange={ageChangeHandler}
+            value={enteredAge}
+          />
+          <Button type='submit'>Add User</Button>
+        </form>
+      </Card>
+    </div>
   );
 };
 
